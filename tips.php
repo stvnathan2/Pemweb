6e00e4b91bb676b2da290469136f5b55f2d57acd
LahKokAku? <danzidandan20@gmail.com>
2024-06-05 01:45:38 +0700

    Add files via upload

---
+++ tips.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Money Mastery - Tips</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h2 class="logo">Money Mastery</h2>
+        <nav class="navigation">
+            <a href="index.php">Beranda</a>
+            <a href="tips.php">Tips</a>
+            <a href="konsultasi.php">Konsultasi</a>
+            <a href="tentang.php">Tentang</a>
+            <a href="bantuan.php">Bantuan</a>
+        </nav>
+    </header>
+
+    <main>
+        <div class="container-index">
+            <section class="hero">
+                <div class="hero-content">
+                    <h1>Tips Pengelolaan Keuangan</h1>
+                    <div class="tips">
+                        <div class="tip">
+                            <h2 class="tip-title">Menabung adalah Kunci</h2>
+                            <p class="tip-description">Mulailah menabung sejak dini. Simpanan kecil dapat menjadi besar seiring berjalannya waktu.</p>
+                        </div>
+                        <div class="tip">
+                            <h2 class="tip-title">Buat Anggaran</h2>
+                            <p class="tip-description">Buatlah anggaran keuangan bulanan dan patuhi. Ini membantu Anda untuk mengelola uang dengan lebih baik.</p>
+                        </div>
+                        <div class="tip">
+                            <h2 class="tip-title">Hindari Utang yang Tidak Perlu</h2>
+                            <p class="tip-description">Hindari utang konsumtif yang tidak diperlukan. Utang yang berlebihan dapat memberatkan keuangan Anda.</p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </main>
+    
+    <footer>
+        <p>&copy; 2024 Money Mastery. All rights reserved.</p>
+    </footer>
+</body>
+</html>
